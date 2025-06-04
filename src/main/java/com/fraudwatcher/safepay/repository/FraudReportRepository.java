b1e6f39b3845e92c1165ea072284afd79f4619a2
eesquilinv <e.esquilinv@gmail.com>
2025-06-04 14:21:13 -0400

    Implement FraudReport repository and service with createFraudReport method for fraud reporting functionality

--- src/main/java/com/fraudwatcher/safepay/repository/FraudReportRepository.java
+++ src/main/java/com/fraudwatcher/safepay/repository/FraudReportRepository.java
@@ -1,4 +1,11 @@
 package com.fraudwatcher.safepay.repository;
 
-public interface FraudReportRepository {
+import com.fraudwatcher.safepay.model.FraudReport;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FraudReportRepository extends JpaRepository<FraudReport, Long> {
+
+
 }
--- src/main/java/com/fraudwatcher/safepay/service/FraudReportService.java
+++ src/main/java/com/fraudwatcher/safepay/service/FraudReportService.java
@@ -1,4 +1,36 @@
 package com.fraudwatcher.safepay.service;
 
+import com.fraudwatcher.safepay.model.FraudReport;
+import com.fraudwatcher.safepay.model.Transaction;
+import com.fraudwatcher.safepay.repository.FraudReportRepository;
+import com.fraudwatcher.safepay.repository.TransactionRepository;
+import jakarta.persistence.EntityNotFoundException;
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.stereotype.Service;
+
+@Service
 public class FraudReportService {
+
+    @Autowired
+    FraudReportRepository fraudReportRepository;
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+    public FraudReport createFraudReport(Long transactionId, String reason) {
+        Transaction transaction = transactionRepository.findById(transactionId)
+                .orElseThrow(() -> new EntityNotFoundException("Transaction ID" + transactionId + " not found."));
+
+        FraudReport report = new FraudReport();
+        report.setDetectedAt(LocalDateTime.now());
+        report.setTransactionId(transaction.getId());
+        report.setUserId(transaction.getUserId());
+        report.setReason(reason);
+        
+        return fraudReportRepository.save(report);
+
+    }
 }
