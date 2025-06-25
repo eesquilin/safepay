d3d4e2dd908a00cbf1d89e848ae48b0811e69777
eesquilinv <e.esquilinv@gmail.com>
2025-06-24 20:09:55 -0400

    Remove trailing slash from TransactionController request mapping

--- src/main/java/com/fraudwatcher/safepay/controller/TransactionController.java
+++ src/main/java/com/fraudwatcher/safepay/controller/TransactionController.java
@@ -15,13 +15,14 @@ import com.fraudwatcher.safepay.model.FraudCheckResult;
 import com.fraudwatcher.safepay.model.Transaction;
 import com.fraudwatcher.safepay.service.FraudCheckService;
 import com.fraudwatcher.safepay.service.TransactionService;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
-@RequestMapping("/api/transactions/")
+@RequestMapping("/api/transactions")
 public class TransactionController {
 
     final TransactionService transactionService;
@@ -70,5 +71,7 @@ public class TransactionController {
         return ResponseEntity.ok(responseDTO);
 
     }
+
+
 
 }
