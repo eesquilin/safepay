3baa68f8f597e339bda2dbff76fa95bc722440e5
eesquilinv <e.esquilinv@gmail.com>
2025-06-28 10:36:38 -0400

    Refactor imports in TransactionController: replace javax.validation with jakarta.validation and organize import statements

--- src/main/java/com/fraudwatcher/safepay/controller/TransactionController.java
+++ src/main/java/com/fraudwatcher/safepay/controller/TransactionController.java
@@ -2,12 +2,14 @@ package com.fraudwatcher.safepay.controller;
 
 import java.util.List;
 
-import javax.validation.Valid;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -20,10 +22,8 @@ import com.fraudwatcher.safepay.model.Transaction;
 import com.fraudwatcher.safepay.service.FraudCheckService;
 import com.fraudwatcher.safepay.service.TransactionService;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import jakarta.validation.Valid;
+
 
 @RestController
 @RequestMapping("/api/transactions")
