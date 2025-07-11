313066c1def7da4b49be138909a0bf96aa70e426
eesquilinv <e.esquilinv@gmail.com>
2025-07-11 18:12:46 -0400

    Refactor TransactionService: enhance logging for transaction operations and replace System.out with logger for fraud detection

--- src/main/java/com/fraudwatcher/safepay/service/TransactionService.java
+++ src/main/java/com/fraudwatcher/safepay/service/TransactionService.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -20,22 +22,27 @@ public class TransactionService {
 
     final FraudCheckService fraudCheckService;
 
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
     TransactionService(TransactionRepository transactionRepository, FraudCheckService fraudCheckService) {
         this.transactionRepository = transactionRepository;
         this.fraudCheckService = fraudCheckService;
     }
 
     public List<Transaction> getAllTransactions() {
+        logger.debug("Fetching all transactions...");
         return transactionRepository.findAll();
     }
 
     public Optional<Transaction> getTransactionById(Long transactionId) {
+        logger.debug("Fetching transaction {}.", transactionId);
         return transactionRepository.findById(transactionId);
     }
 
     // get all transactions of a certain user
     public Page<Transaction> getTransactionByUserId(Long userId, Pageable pageable) {
         return transactionRepository.findByUserId(userId, pageable);
+        
     }
 
     // method to create transaction and detect possible fraud during transaction
@@ -43,11 +50,13 @@ public class TransactionService {
     public Transaction createTransaction(Transaction transaction) {
         transaction.setTimestamp(LocalDateTime.now());
         Transaction savedTransaction = transactionRepository.save(transaction);
+        logger.info("Transaction posted for userId: {}", transaction.getUserId());
 
         FraudCheckResult result = fraudCheckService.evaluateTransaction(savedTransaction.getId());
 
         if (result.isFraud()) {
-            System.out.printf("Potential fraud detected for transaction %d.%n", savedTransaction.getId());
+            // System.out.printf("Potential fraud detected for transaction %d.%n", savedTransaction.getId());
+            logger.warn("Potential fraud detected for transaction {}.", savedTransaction.getId());
         }
 
         return savedTransaction;
